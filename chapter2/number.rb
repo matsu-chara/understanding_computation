0dfd38fbad7ee6318b07949da192c0ba244a3804
matsu-chara <matsuy00@gmail.com>
2014-11-21 21:54:12 +0900

    whileを追加

--- chapter2/number.rb
+++ chapter2/number.rb
@@ -51,42 +51,11 @@ class Multiply < Struct.new(:left, :right)
 
   def reduce(environment)
     if left.reducible?
-      Add.new(left.reduce(environment), right)
+      Multiply.new(left.reduce(environment), right)
     elsif right.reducible?
-      Add.new(left, right.reduce(environment))
+      Multiply.new(left, right.reduce(environment))
     else
       Number.new(left.value * right.value)
     end
   end
 end
-
-# 抽象構文木(AST)の手動構築
-# （）をつけたりしないので、出力された式は必ずしも正しくない。
-# p Add.new(
-#   Multiply.new(Number.new(1), Number.new(2)),
-#   Multiply.new(Number.new(3), Number.new(4))
-# )
-
-# 簡約可能かどうか
-# p Number.new(1).reducible?
-# p Add.new(Number.new(1), Number.new(2)).reducible?
-
-# 簡約実行
-# e =  Add.new(
-#   Multiply.new(Number.new(1), Number.new(2)),
-#   Multiply.new(Number.new(3), Number.new(4))
-# )
-# p e
-# p e.reducible?
-# e = e.reduce
-#
-# p e
-# p e.reducible?
-# e = e.reduce
-#
-# p e
-# p e.reducible?
-# e = e.reduce
-#
-# p e
-# p e.reducible?
--- chapter2/vm.rb
+++ chapter2/vm.rb
@@ -5,10 +5,12 @@ require './nothing.rb'
 require './assign.rb'
 require './if.rb'
 require './sequence.rb'
+require './while.rb'
 
 class Machine < Struct.new(:statement, :environment)
   def step
-    self.statement, self.environment = statement.reduce(environment)
+    self.statement, e = statement.reduce(environment)
+    self.environment = e if e
   end
 
   def run
@@ -20,28 +22,30 @@ class Machine < Struct.new(:statement, :environment)
   end
 end
 
-# Machine.new(
-#   Add.new(
-#     Multiply.new(Number.new(1), Number.new(2)),
-#     Multiply.new(Number.new(3), Number.new(4))
-#   )
-# ).run
-#
-# puts ""
-# Machine.new(
-#   LessThan.new(
-#     Number.new(5),
-#     Add.new(Number.new(2), Number.new(2))
-#   )
-# ).run
-#
-# puts ""
-# Machine.new(
-#   Add.new(Variable.new(:x), Variable.new(:y)),
-#   { x: Number.new(3), y: Number.new(4) }
-# ).run
+Machine.new(
+  Add.new(
+    Multiply.new(Number.new(1), Number.new(2)),
+    Multiply.new(Number.new(3), Number.new(4))
+  ),
+  {}
+).run
 
-# puts ""
+puts ""
+Machine.new(
+  LessThan.new(
+    Number.new(5),
+    Add.new(Number.new(2), Number.new(2))
+  ),
+  {}
+).run
+
+puts ""
+Machine.new(
+  Add.new(Variable.new(:x), Variable.new(:y)),
+  { x: Number.new(3), y: Number.new(4) }
+).run
+
+puts ""
 Machine.new(
   Assign.new(:x, Add.new(Variable.new(:x), Number.new(1))),
   { x: Number.new(2) }
@@ -60,8 +64,17 @@ Machine.new(
 puts ""
 Machine.new(
   Sequence.new(
-  Assign.new(:x, Add.new(Number.new(1), Number.new(1))),
-  Assign.new(:y, Add.new(Variable.new(:x), Number.new(3)))
+    Assign.new(:x, Add.new(Number.new(1), Number.new(1))),
+    Assign.new(:y, Add.new(Variable.new(:x), Number.new(3)))
   ),
   {}
+).run
+
+puts ""
+Machine.new(
+  While.new(
+    LessThan.new(Variable.new(:x), Number.new(5)),
+    Assign.new(:x, Multiply.new(Variable.new(:x), Number.new(3)))
+  ),
+  { x: Number.new(1) }
 ).run

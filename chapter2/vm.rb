61f228283045c3c07cfd5f119b64fadac06f9df4
matsu-chara <matsuy00@gmail.com>
2014-11-21 20:20:54 +0900

    代入を追加

--- chapter2/vm.rb
+++ chapter2/vm.rb
@@ -1,40 +1,46 @@
 require './number.rb'
 require './bool.rb'
 require './variable.rb'
+require './nothing.rb'
+require './assign.rb'
 
-class Machine < Struct.new(:expression, :environment)
+class Machine < Struct.new(:statement, :environment)
   def step
-    self.expression = expression.reduce(environment)
+    self.statement, self.environment = statement.reduce(environment)
   end
 
   def run
-    while expression.reducible?
-      puts expression
+    while statement.reducible?
+      puts "#{statement}, #{environment}"
       step
     end
-    puts expression
+    puts "#{statement}, #{environment}"
   end
 end
 
-Machine.new(
-  Add.new(
-    Multiply.new(Number.new(1), Number.new(2)),
-    Multiply.new(Number.new(3), Number.new(4))
-  )
-).run
+# Machine.new(
+#   Add.new(
+#     Multiply.new(Number.new(1), Number.new(2)),
+#     Multiply.new(Number.new(3), Number.new(4))
+#   )
+# ).run
+#
+# puts ""
+# Machine.new(
+#   LessThan.new(
+#     Number.new(5),
+#     Add.new(Number.new(2), Number.new(2))
+#   )
+# ).run
+#
+# puts ""
+# Machine.new(
+#   Add.new(Variable.new(:x), Variable.new(:y)),
+#   { x: Number.new(3), y: Number.new(4) }
+# ).run
 
 puts ""
-
 Machine.new(
-  LessThan.new(
-    Number.new(5),
-    Add.new(Number.new(2), Number.new(2))
-  )
-).run
-
-puts ""
-
-Machine.new(
-  Add.new(Variable.new(:x), Variable.new(:y)),
-  { x: Number.new(3), y: Number.new(4) }
+  Assign.new(:x, Add.new(Variable.new(:x), Number.new(1))),
+  { x: Number.new(2) }
 ).run

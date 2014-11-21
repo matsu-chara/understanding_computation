07ed2fce5610370300d918e5534d9e75c37d31a8
matsu-chara <matsuy00@gmail.com>
2014-11-21 18:27:08 +0900

    手動による式の簡約

--- chapter2/simple.rb
+++ chapter2/simple.rb
@@ -6,6 +6,10 @@ class Number < Struct.new(:value)
   def inspect
     "<<#{self}>>"
   end
+
+  def reducible?()
+      false
+  end
 end
 
 class Add < Struct.new(:left, :right)
@@ -15,6 +19,20 @@ class Add < Struct.new(:left, :right)
 
   def inspect
     "<<#{self}>>"
+  end
+
+  def reducible?()
+      true
+  end
+
+  def reduce
+    if left.reducible?
+      Add.new(left.reduce, right)
+    elsif right.reducible?
+      Add.new(left, right.reduce)
+    else
+      Number.new(left.value + right.value)
+    end
   end
 end
 
@@ -26,11 +44,49 @@ class Multiply < Struct.new(:left, :right)
   def inspect
     "<<#{self}>>"
   end
+
+  def reducible?()
+      true
+  end
+
+  def reduce
+    if left.reducible?
+      Add.new(left.reduce, right)
+    elsif right.reducible?
+      Add.new(left, right.reduce)
+    else
+      Number.new(left.value * right.value)
+    end
+  end
 end
 
 # 抽象構文木(AST)の手動構築
 # （）をつけたりしないので、出力された式は必ずしも正しくない。
-p Add.new(
-  Multiply.new(Number.new(1), Number.new(2)),
-  Multiply.new(Number.new(3), Number.new(4))
-)
+# p Add.new(
+#   Multiply.new(Number.new(1), Number.new(2)),
+#   Multiply.new(Number.new(3), Number.new(4))
+# )
+
+# 簡約可能かどうか
+# p Number.new(1).reducible?
+# p Add.new(Number.new(1), Number.new(2)).reducible?
+
+# 簡約実行
+# e =  Add.new(
+#   Multiply.new(Number.new(1), Number.new(2)),
+#   Multiply.new(Number.new(3), Number.new(4))
+# )
+# p e
+# p e.reducible?
+# e = e.reduce
+#
+# p e
+# p e.reducible?
+# e = e.reduce
+#
+# p e
+# p e.reducible?
+# e = e.reduce
+#
+# p e
+# p e.reducible?

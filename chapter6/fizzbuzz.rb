60739b1abdbe9d9b3caed14b68815fea9c4f38ac
matsu-chara <matsuy00@gmail.com>
2014-11-27 02:03:41 +0900

    加減乗除

--- chapter6/fizzbuzz.rb
+++ chapter6/fizzbuzz.rb
@@ -22,3 +22,43 @@ IS_ZERO = -> n { n[-> x { FALSE }][TRUE] }
 PAIR  = -> x { -> y { -> f { f[x][y] } } }
 LEFT  = -> p { p[-> x { -> y { x } } ] }
 RIGHT = -> p { p[-> x { -> y { y } } ] }
+
+INCREMENT = -> n { -> p { -> { p[n][p][x] } } }
+SLIDE     = -> p { PAIR[RIGHT[p]][INCREMENT[RIGHT[p]]] }
+DECREMENT = -> n { LEFT[n[SLIDE][PAIR[ZERO][ZERO]]] }
+
+ADD       = -> m { -> n { n[INCREMENT][m] } }
+SUBTRACT  = -> m { -> n { n[DECREMENT][m] } }
+MULTIPLY  = -> m { -> n { n[ADD[m]][ZERO] } }
+POWER     = -> m { -> n { n[MULTIPLY[m]][ONE] } }
+
+# 答えが負の数になるような引き算をしても0が帰ってくることを利用している
+IS_LESS_OR_EQUAL =
+  -> m { -> n {
+    IS_ZERO[SUBTRACT[m][n]]
+} }
+
+# 再帰版
+# MOD =
+#   -> m { -> n {
+#     IF[IS_LESS_OR_EQUAL[m][n]][
+#       -> x {
+#         MOD[SUBTRACT[m][n]][n][x]
+#       }
+#     ][
+#       m
+#     ]
+# } }
+
+# 再帰使わない版
+Z = -> f { -> x { f[->y { x[x][y] }] }[-> x {f[-> y { x[x][y] }] }] }
+MOD =
+  Z[->f {-> m { -> n {
+    IF[IS_LESS_OR_EQUAL[m][n]][
+      -> x {
+        f[SUBTRACT[m][n]][n][x]
+      }
+    ][
+      m
+    ]
+} } }]
